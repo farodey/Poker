ebe04e12afecbe22fa450bd2916c8bb03c6cdc9c
farodey <farodey@yandex.ru>
2020-10-07 15:10:36 +0300

    add window

--- HoldemHanrDistribution.py
+++
@@ -1,2 +0,0 @@
-class HoldemHandDistribution:
-    pass
--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app = QApplication(sys.argv)
+
+    w = QWidget()
+    w.resize(400, 300)
+    w.move(1200, 200)
+    w.setWindowTitle('Poker')
+    w.show()
+
+    sys.exit(app.exec_())
